46f0e75fd6569c80b7ec6fcc5ecccf317eb9419c
unknown <leandromoh@hotmail.com>
2023-06-12 06:46:50 -0300

    change tests

--- test/CNPJTest.cs
+++ test/CNPJTest.cs
@@ -1,37 +1,19 @@
-namespace DocumentoHelper.Test
+using System.Linq;
+using System.Collections.Generic;
+using Xunit;
+using FluentAssertions;
+
+namespace DocumentoHelper.Test;
+
+public class CNPJTest
 {
-    using NUnit.Framework;
-    using DocumentoHelper;
-    using System.Linq;
-    using System.Collections.Generic;
+    [Theory]
+    [MemberData(nameof(GenerateCNPJSource))]
+    public void Validate(string doc) =>
+        CNPJ.Validate(doc).Should().BeTrue();
 
-    [TestFixture]
-    public class CNPJTest
-    {
-
-        [TestCaseSource(nameof(GenerateCNPJSource))]
-        public void GenerateCNPJ(long cnpj) => Assert.IsTrue(CNPJ.IsValid(cnpj));
-		
-        public static IEnumerable<object> GenerateCNPJSource() =>
-            Enumerable.Range(0, 100).Select(_ => CNPJ.Generate()).Cast<object>();
-
-        [Test]
-        public void CalculateCheckDigits() => 
-            Assert.AreEqual(new[] { 2,1 }, CNPJ.CalculateCheckDigits(new[] { 4,4,2,6,2,6,3,7,0,0,0,1 }));
-
-        [Test]
-        public void IsValidString() => Assert.IsTrue(CNPJ.IsValid("44.262.637/0001-21"));
-
-        [Test]
-        public void IsValidLong() => Assert.IsTrue(CNPJ.IsValid(44262637000121));
-
-        [Test]
-        public void IsValidIntArray() =>  Assert.IsTrue(CNPJ.IsValid(new[] { 4,4,2,6,2,6,3,7,0,0,0,1,2,1 }));
-
-        [Test]
-        public void FormatString() => Assert.AreEqual(CNPJ.Format("44262637000121"), "44.262.637/0001-21");
-
-        [Test]
-        public void FormatLong() => Assert.AreEqual(CNPJ.Format(44262637000121), "44.262.637/0001-21");
-    }
+    public static IEnumerable<object[]> GenerateCNPJSource() =>
+        Enumerable
+        .Range(0, 1000)
+        .Select(_ => new[] { CNPJ.GenerateUnformatted() });
 }
